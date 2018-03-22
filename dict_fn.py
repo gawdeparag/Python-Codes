b09db3229bed71b32ac26f88a6049948cea3e5c2
Parag S Gawde <paraggawde@kccemsr.edu.in>
2018-03-22 07:13:35 +0530

    Adding program depicting dictionary functions

---
+++ dict_fn.py
@@ -0,0 +1,13 @@
+# Initializing dictionary
+dic1 = { 'Name' : 'MySQL', 'Server' : 'XAMPP', 'Stack': 'LAMP' }
+dic3 = {}
+# using copy() to make shallow copy of dictionary
+dic3 = dic1.copy()
+# printing new dictionary
+print ("The new copied dictionary is : ")
+print (dic3.items())
+dic1.clear()
+# printing cleared dictionary
+print ("The contents of deleted dictionary is : ")
+print (dic1.items())
+
