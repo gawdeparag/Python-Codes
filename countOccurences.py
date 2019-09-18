ad8a05c43dccac7cc1642a42fa29cbf887014a91
paraggawde <paraggawde.303@gmail.com>
2019-09-18 17:02:37 +0530

    lapindrome incomplete

--- countOccurences.py
+++ countOccurences.py
@@ -1,8 +1,5 @@
 arr = [0, 1, 2, 3, 4, 5, 4, 3, 2, 1]
-
 for i in range(0, len(arr)):
-    counter = 0
-    for j in range(0, i+1):
-        if(arr[i] == arr[j]):
-            counter += 1      
-    print("Element {0} occurs {1} times".format(arr[i], counter))
+    occurence = arr.count(arr[i])
+    print("Element {0} occurs {1} times".format(arr[i], occurence))   
+
---
+++ lapindrome.py
@@ -0,0 +1,4 @@
+string = "gaga"
+
+print(list(string))
+
